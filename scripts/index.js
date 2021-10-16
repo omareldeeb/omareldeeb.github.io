3da6e98213bb66abe0db61c6d131972feb7f0f57
omareldeeb <omareldeeb00@live.com>
2021-10-16 16:26:31 +0200

    Mobile design enhancements

--- scripts/index.js
+++ scripts/index.js
@@ -20,10 +20,17 @@ Array.from(document.getElementsByClassName('fade-on-scroll')).forEach(el => {
 
 redrawSine(1.0, 1.0);
 
-function redrawSine(frequencyMultiplier, amplitudeMultiplier) {
-    // adapted from https://stackoverflow.com/questions/29917446/drawing-sine-wave-in-canvas
+// Copied from https://stackoverflow.com/questions/4817029/whats-the-best-way-to-detect-a-touch-screen-device-using-javascript
+function isTouchDevice() {
+    return (('ontouchstart' in window) ||
+       (navigator.maxTouchPoints > 0) ||
+       (navigator.msMaxTouchPoints > 0));
+}
 
-    // full width
+function redrawSine(frequencyMultiplier, amplitudeMultiplier) {
+    // Adapted from https://stackoverflow.com/questions/29917446/drawing-sine-wave-in-canvas
+
+    // Full width
     var targetWidth = document.body.clientWidth;
     var c = document.getElementById('sineCanvas');
     c.width = targetWidth;
@@ -46,22 +53,44 @@ function redrawSine(frequencyMultiplier, amplitudeMultiplier) {
 }
 
 window.addEventListener('mousemove', e => {
-    var width = document.body.clientWidth;
-    var height = document.body.clientHeight / 2;
+    var width = window.innerWidth;
+    var height = window.innerHeight;
 
     var relativeX = e.pageX / width;
     var frequencyMultiplier = 1 - relativeX + 0.5;
 
     var relativeY = e.pageY / height;
-    var amplitudeMultiplier = 1.0 - relativeY * 2.0;
+    var amplitudeMultiplier = 1.0 - 2.0 * relativeY;
     
     // Stop amplitude from getting infinitely big when scrolling down
     if (amplitudeMultiplier < -1.0) {
         amplitudeMultiplier = -1.0;
     }
-    // if (frequencyMultiplier < 0.1) {
-    //     frequencyMultiplier = 0.1
-    // }
+
+    if (frequencyMultiplier < 0.5) {
+        frequencyMultiplier = 0.5;
+    }
     
     redrawSine(frequencyMultiplier, amplitudeMultiplier);
 });
+
+// Manipulate sine wave on scroll on touch screens
+if (isTouchDevice()) {
+    window.addEventListener('scroll', e => {
+        var height = window.innerHeight / 2;
+        var relativeValue = window.scrollY / (height + 150);
+    
+        var frequencyMultiplier = 1.0 - relativeValue + 0.5;
+        var amplitudeMultiplier = 1.0 - 2.0 * relativeValue;
+    
+        if (amplitudeMultiplier < -1.0) {
+            amplitudeMultiplier = -1.0;
+        }
+    
+        if (frequencyMultiplier < 0.5) {
+            frequencyMultiplier = 0.5;
+        }
+    
+        redrawSine(frequencyMultiplier, amplitudeMultiplier);
+    });
+}
